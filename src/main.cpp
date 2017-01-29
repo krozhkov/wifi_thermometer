3a9d29b2418cd23dce7405bee9f9780ec5d61d61
Rozhkov Konstantin <rkv86@live.ru>
2017-01-29 13:39:41 +0700

    Добавил экранный буфер

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include "LedControl.h"
 
-LedControl lc = LedControl(D3,D1,D2,2);
+LedControl lc = LedControl(D3, D1, D2, 2);
 
-unsigned long delaytime=1000;
+unsigned long delaytime = 1000;
 
-static const byte minus[] PROGMEM = {B00000000, B00000000, B00000000, B00000000, B00000111, B00000000, B00000000, B00000000};
-static const byte digits[10][8] = {
+static const unsigned char minus[] = {B00000000, B00000000, B00000000, B00000000, B00000111, B00000000, B00000000, B00000000};
+static const unsigned char digits[10][8] = {
   {B00000000, B00001110, B00010001, B00010011, B00010101, B00011001, B00010001, B00001110},
   {B00000000, B00000100, B00001100, B00010100, B00000100, B00000100, B00000100, B00011111},
   {B00000000, B00001110, B00010001, B00010001, B00000110, B00001000, B00010000, B00011111},
@@ -19,28 +19,64 @@ static const byte digits[10][8] = {
   {B00000000, B00001110, B00010001, B00010001, B00001111, B00000001, B00010001, B00001110}
 };
 
-void setup() {
-  for (int i = 0; i < lc.getDeviceCount(); i++) {
-    lc.shutdown(i, false);
-    lc.setIntensity(i, 4);
-    lc.clearDisplay(i);
+unsigned short buffer[8];
+
+void clearBuffer() {
+  for (int i = 0; i < 8; i++) {
+    buffer[i] = 0;
   }
 }
 
-void writeArduinoOnMatrix() {
-  for (int num = 0; num < 10; num++) {
-    lc.setRow(0,0,digits[num][0]);
-    lc.setRow(0,1,digits[num][1]);
-    lc.setRow(0,2,digits[num][2]);
-    lc.setRow(0,3,digits[num][3]);
-    lc.setRow(0,4,digits[num][4]);
-    lc.setRow(0,5,digits[num][5]);
-    lc.setRow(0,6,digits[num][6]);
-    lc.setRow(0,7,digits[num][7]);
-    delay(delaytime);
+void shiftBuffer(int offset) {
+  for (int i = 0; i < 8; i++) {
+    buffer[i] <<= offset;
   }
+}
+
+void flushBuffer() {
+  for (int i = 0; i < 8; i++) {
+    unsigned char right = buffer[i];
+    unsigned char left = buffer[i] >> 8;
+    lc.setRow(0, i, right);
+    lc.setRow(1, i, left);
+  }
+}
+
+void dumpBuffer() {
+  for (int i = 0; i < 8; i++) {
+    Serial.println(String(buffer[i], BIN));
+  }
+}
+
+void writeDigit(int num) {
+  shiftBuffer(6);
+  for (int i = 0; i < 8; i++) {
+    buffer[i] = buffer[i] | digits[num][i];
+  }
+}
+
+void writeMinus() {
+  shiftBuffer(4);
+  for (int i = 0; i < 8; i++) {
+    buffer[i] = buffer[i] | minus[i];
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  for (int i = 0; i < lc.getDeviceCount(); i++) {
+    lc.shutdown(i, false);
+    lc.setIntensity(i, 2);
+    lc.clearDisplay(i);
+  }
+  clearBuffer();
+  writeMinus();
+  writeDigit(2);
+  writeDigit(5);
+  shiftBuffer(1);
+  flushBuffer();
 }
 
 void loop() {
-  writeArduinoOnMatrix();
 }
